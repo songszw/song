f15237ca939ab1991bd89367f0baf17a77bde34e
songszw <songszw@gmail.com>
2017-10-17 17:05:39 +0800

    添加xadmin后台面板

--- myblog/apps/users/admin.py
+++ myblog/apps/users/admin.py
@@ -1,3 +1,10 @@
+# _*_ coding:utf-8 _*_
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+#
+#
+# class UserProfileAdmin(admin.ModelAdmin):
+#     pass
+#
+# admin.site.register(UserProfile,UserProfileAdmin)
