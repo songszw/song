f5e9c650254794dd550fa44cf362c76967f4b16b
songszw <songszw@gmail.com>
2017-10-20 16:41:39 +0800

    编写登陆页面，用户名与邮箱均可登录

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -17,7 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
+
+from users.views import userLogin
+
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
-    url(r'^$',TemplateView.as_view(template_name='index.html'),name='index')
+    url(r'^$',TemplateView.as_view(template_name='index.html'),name='index'),
+    url(r'^login/$',userLogin,name='login')
 ]
