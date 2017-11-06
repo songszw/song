270421b50d3da003010d3c1e3c3921a4a0ad2b36
songszw <songszw@gmail.com>
2017-11-06 16:58:26 +0800

    我要咨询功能（有bug）

--- myblog/apps/organization/forms.py
+++ myblog/apps/organization/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from operation.models import UserAsk
 
 
-class UserAskForm(forms.Form):
+class UserAskForm(forms.ModelForm):
     class Meta:
-        module = UserAsk
+        model = UserAsk
         fields = ['name','mobile','course_name']
