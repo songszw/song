d7504df77827503c00f2dadfdd5cfba7c45f3c7a
songszw <songszw@gmail.com>
2017-11-16 16:36:34 +0800

    添加登陆验证机制

--- myblog/apps/courses/views.py
+++ myblog/apps/courses/views.py
@@ -4,14 +4,16 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
-from .models import Courses,CourseResource
-from operation.models import UserFavorite,CourseComments
+from .models import Courses, CourseResource
+from operation.models import UserFavorite, CourseComments,UserCourse
+from utils.mixin_utils import LoginRequiredMixin
 
 # Create your views here.
 
 
 class CourseListView(View):
-    def get(self,request):
+    def get(self, request):
+
         all_courses = Courses.objects.all().order_by('-add_time')
 
         hot_courses = Courses.objects.all().order_by('-click_nums')[:3]
@@ -29,19 +31,19 @@ class CourseListView(View):
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
-        p = Paginator(all_courses,6, request=request)
+        p = Paginator(all_courses, 6, request=request)
         courses = p.page(page)
 
-        return render(request,'course-list.html',{
-            'all_courses':courses,
-            'sort':sort,
+        return render(request, 'course-list.html', {
+            'all_courses':courses, 
+            'sort':sort, 
             'hot_courses':hot_courses
         })
 
 
 class CourseDetailView(View):
     # 课程详情页
-    def get(self,request,course_id):
+    def get(self, request, course_id):
         course = Courses.objects.get(id=int(course_id))
         # 增加课程点击数
         course.click_nums += 1
@@ -51,12 +53,11 @@ class CourseDetailView(View):
         has_fav_org = False
 
         if request.user.is_authenticated():
-            if UserFavorite.objects.filter(user = request.user,fav_id=course.id,fav_type=1):
+            if UserFavorite.objects.filter(user = request.user, fav_id=course.id, fav_type=1):
                 has_fav_course = True
 
-            if UserFavorite.objects.filter(user = request.user,fav_id=course.course_org.id,fav_type=2):
+            if UserFavorite.objects.filter(user = request.user, fav_id=course.course_org.id, fav_type=2):
                 has_fav_org = True
-
 
         tag = course.tag
         if tag:
@@ -64,48 +65,55 @@ class CourseDetailView(View):
         else:
             relate_coures = []
 
-
-        return render(request,'course-detail.html',{
-            'course':course,
-            'relate_coures':relate_coures,
-            'has_fav_course':has_fav_course,
+        return render(request, 'course-detail.html', {
+            'course':course, 
+            'relate_coures':relate_coures, 
+            'has_fav_course':has_fav_course, 
             'has_fav_org':has_fav_org
         })
 
 
-class CourseInfoView(View):
+class CourseInfoView(LoginRequiredMixin,View):
     # 课程章节信息
-    def get(self,request,course_id):
-        course = Courses.objects.get(id = int(course_id))
+    def get(self, request, course_id):
+        course = Courses.objects.get(id=int(course_id))
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_course.course.id for user_course in user_courses]
+        # 获取学过该用户的其他所学课程
+        relate_courses = Courses.objects.filter(id__in = course_ids).order_by('-click_nums')[:5]
         all_resources = CourseResource.objects.filter(course = course)
-        return render(request,'course-video.html',{
-            'course':course,
-            'course_resourses':all_resources
+        return render(request, 'course-video.html', {
+            'course':course, 
+            'course_resourses':all_resources,
+            'relate_courses':relate_courses
         })
 
 
-class CourseCommentView(View):
+class CourseCommentView(LoginRequiredMixin,View):
     # 课程评论
     def get(self, request, course_id):
         course = Courses.objects.get(id=int(course_id))
         all_resources = CourseResource.objects.filter(course=course)
-        all_comments = CourseComments.objects.all()
+        all_comments = CourseComments.objects.filter(course_id=course)
         return render(request, 'course-comment.html', {
-            'course': course,
-            'course_resourses': all_resources,
+            'course': course, 
+            'course_resourses': all_resources, 
             'all_comments':all_comments
         })
 
 
 class AddCommentView(View):
     # 用户添加课程评论
-    def post(self,request):
+    def post(self, request):
         # 首先判断用户是否已登陆
         if not request.user.is_authenticated():
-            return HttpResponse('{"status":"fail","msg":"用户未登录"}',content_type='application/json')
+            return HttpResponse('{"status":"fail", "msg":"用户未登录"}', content_type='application/json')
 
         course_id = request.POST.get('course_id', 0)
-        comments = request.POST.get('comments','')
+        comments = request.POST.get('comments', '')
         if course_id > 0 and comments:
             course_comments = CourseComments()
             course = Courses.objects.get(id = int(course_id))
@@ -113,9 +121,9 @@ class AddCommentView(View):
             course_comments.comments = comments
             course_comments.user = request.user
             course_comments.save()
-            return HttpResponse('{"status":"success","msg":"添加成功"}',content_type='application/json')
+            return HttpResponse('{"status":"success", "msg":"添加成功"}', content_type='application/json')
         else:
-            return HttpResponse('{"status":"success","msg":"添加失败"}',content_type='application/json')
+            return HttpResponse('{"status":"success", "msg":"添加失败"}', content_type='application/json')
 
 
 
