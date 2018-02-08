e050d9643d1ae5cc151e3743265f15670528de4d
songszw <songszw@gmail.com>
2018-02-08 16:47:30 +0800

    添加500页面

--- p3scrapy/regtest/test/test.py
+++ p3scrapy/regtest/test/test.py
@@ -2,11 +2,15 @@
 
 import re
 
-line = 'bobby123'
+line = 'xxx出生于2011年1月3号'
+line = 'xxx出生于2011年1月'
+line = 'xxx出生于2011/1/3'
+line = 'xxx出生于2011-1-3'
+line = 'xxx出生于2011-1'
+line = 'xxx出生于2011/01/03'
+line = 'xxx出生于2011-01-03'
 
-regex_str = '^.*3$'
-
-if re.match(regex_str,line):
-    print('yes')
-else:
-    print('no')
+regex_str = ".*出生于(\d{4}[年/-]\d{1,2}([月/-]\d{1,2}|[月/-]$|$))"
+macth_obj = re.match(regex_str,line)
+if macth_obj:
+    print(macth_obj.group(1))
